32ccd5cd41f6a551f696fb0542ac8d96f2fba8c1
Vasiliy <void.pointer@ya.ru>
2023-06-07 02:31:31 +0500

    fix: attempt to fix graalvm native image issue

--- src/main/java/voidpointer/daemon/sshnotify/config/TelegramConfig.java
+++ src/main/java/voidpointer/daemon/sshnotify/config/TelegramConfig.java
@@ -1,19 +1,22 @@
 package voidpointer.daemon.sshnotify.config;
 
-import lombok.NoArgsConstructor;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 import java.nio.file.Path;
 
-@NoArgsConstructor
 @ConfigSerializable
 public final class TelegramConfig {
     public static TelegramConfig loadAndSave(final Path src) {
         return voidpointer.daemon.sshnotify.config.HoconConfigLoader.loadAndSave(src, TelegramConfig.class, TelegramConfig::new);
     }
 
-    private String token = "";
-    private String botUsername = "";
+    private String token;
+    private String botUsername;
+
+    public TelegramConfig() {
+        this.token = "";
+        this.botUsername = "";
+    }
 
     public String token() {
         return token;
